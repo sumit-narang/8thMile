884efdbe027965b313dc85442e43c0a3c65b7f75
Sumit Narang <sumitnarang.web@gmail.com>
2017-04-25 23:48:00 +0530

    Added index file

---
+++ typewriter.js
@@ -0,0 +1,20 @@
+var text = $('.typewriter').text();
+
+var length = text.length;
+var timeOut;
+var character = 0;
+
+
+(function typeWriter() { 
+    timeOut = setTimeout(function() {
+        character++;
+        var type = text.substring(0, character);
+        $('.typewriter').text(type);
+        typeWriter();
+        
+        if (character == length) {
+            clearTimeout(timeOut);
+        }
+        
+    }, 200);
+}());
